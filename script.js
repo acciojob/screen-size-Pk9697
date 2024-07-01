28780403e0be4d4e1333f8d7e5acfda3ae552477
Prashant Kumar <38308359+Pk9697@users.noreply.github.com>
2024-07-01 22:49:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+const size=document.getElementById('sizeInfo')
+window.onresize=()=>{
+	size.textContent=`Width: ${window.innerWidth} and Height: ${window.innerHeight}`
+}
